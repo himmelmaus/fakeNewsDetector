6c558b06ffc33bd002c44b9c8401ba4cd782ca07
Karolishp <31632511+KaroliShp@users.noreply.github.com>
2017-10-28 08:48:42 +0100

    Scoring system + minor changes in sentiment_analysis

--- article_topics.py
+++ article_topics.py
@@ -64,6 +64,7 @@ class OnStart:
 
 		# Decending sorting order
 		most_popular_topics = sorted(pairs_dictionary, key=pairs_dictionary.get, reverse=True)
+		print(most_popular_topics)
 		return most_popular_topics
 
 	# Used to find names and any other "PERSON" tags in the text
@@ -84,4 +85,4 @@ class OnStart:
 		return names
 
 if __name__ == "__main__":
-	OnStart()
+	OnStart.FindTopics()
