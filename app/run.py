17e43f16ee9310c70edfda8d4b19146afd026596
findlaysmith <ferelach45@gmail.com>
2017-10-28 11:33:07 +0100

    added comments to run.py

--- app/run.py
+++ app/run.py
@@ -5,7 +5,7 @@ from time import sleep
 
 app = Flask(__name__)
 
-def real(n):
+def real(n): #placeholder function before parsing url input
     try:
         n = int(n)
     except:
@@ -20,10 +20,10 @@ def real(n):
 
 @app.route('/', methods=['GET','POST'])
 def print_form():
-    if request.method == 'POST':
+    if request.method == 'POST': #code to be executed when submit button is pushed
         result = request.form['input']                                                                                                                         
         return render_template('index.html', result=real(result))
-    if request.method == 'GET':
+    if request.method == 'GET': #base text
         return render_template('index.html')
 
 if __name__ == '__main__':
