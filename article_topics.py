a01c4351489d4d5a794382ef0f3de5f2ef2ccd87
Karolishp <31632511+KaroliShp@users.noreply.github.com>
2017-10-28 04:12:10 +0100

    Add files via upload

--- article_topics.py
+++ article_topics.py
@@ -6,59 +6,62 @@ import string
 import gensim # pip3 install Gensim
 from gensim import corpora
 
-# Main code from: https://www.analyticsvidhya.com/blog/2016/08/beginners-guide-to-topic-modeling-in-python/
+# Base code from: https://www.analyticsvidhya.com/blog/2016/08/beginners-guide-to-topic-modeling-in-python/
+# Outputs around 6 words that describes the topic of the article
 
-# Present the article in a sentence per new line
-doc_complete = open("text.txt", "r").readlines()
+def FindTopics():
+	# Present the article in a sentence per new line
+	doc_complete = open("text.txt", "r").readlines()
 
-stop = set(stopwords.words('english'))
-exclude = set(string.punctuation)
-lemma = WordNetLemmatizer()
-unnecessary = []
+	stop = set(stopwords.words('english'))
+	exclude = set(string.punctuation)
+	lemma = WordNetLemmatizer()
+	unnecessary = []
 
-# Pos tag words and remove tags "IN", "MD", "CD"
-for i in range (0, len(doc_complete)):
-	doc_tokens = nltk.word_tokenize(doc_complete[i])
-	doc_pos_tags = nltk.pos_tag(doc_tokens)
-	for i in range (0, len(doc_pos_tags)):
-		if doc_pos_tags[i][1] in ["IN", "MD", "CD", "PDT", "PRP"]:
-			unnecessary.append(doc_pos_tags[i][0])
-		elif doc_pos_tags[i][1] == "NNP":
-			print(doc_pos_tags[i][0])
+	# Pos tag words and remove tags "IN", "MD", "CD"
+	for i in range (0, len(doc_complete)):
+		doc_tokens = nltk.word_tokenize(doc_complete[i])
+		doc_pos_tags = nltk.pos_tag(doc_tokens)
+		for i in range (0, len(doc_pos_tags)):
+			if doc_pos_tags[i][1] in ["IN", "MD", "CD", "PDT", "PRP"]:
+				unnecessary.append(doc_pos_tags[i][0])
 
-# Cleans the composed string from stop words (stop_free), puncuation (punc_free) and l
-def clean(doc):
-    stop_free = " ".join([i for i in doc.lower().split() if i not in stop])
-    unnecessary_free = " ".join([i for i in stop_free.lower().split() if i not in unnecessary])
-    punc_free = ''.join(ch for ch in unnecessary_free if ch not in exclude)
-    normalized = " ".join(lemma.lemmatize(word) for word in punc_free.split())
-    return normalized
+	# Cleans the composed string from stop words (stop_free), puncuation (punc_free) and l
+	def clean(doc):
+	    stop_free = " ".join([i for i in doc.lower().split() if i not in stop])
+	    unnecessary_free = " ".join([i for i in stop_free.lower().split() if i not in unnecessary])
+	    punc_free = ''.join(ch for ch in unnecessary_free if ch not in exclude)
+	    normalized = " ".join(lemma.lemmatize(word) for word in punc_free.split())
+	    return normalized
 
-# Use clean function on doc_complete
-doc_clean = [clean(doc).split() for doc in doc_complete]
+	# Use clean function on doc_complete
+	doc_clean = [clean(doc).split() for doc in doc_complete]
 
-# Matrix representation of a corpus using corpora
-dictionary = corpora.Dictionary(doc_clean)
+	# Matrix representation of a corpus using corpora
+	dictionary = corpora.Dictionary(doc_clean)
 
-# Document - term matrix
-doc_term_matrix = [dictionary.doc2bow(doc) for doc in doc_clean] 
+	# Document - term matrix
+	doc_term_matrix = [dictionary.doc2bow(doc) for doc in doc_clean] 
 
-# Training LDA model
-Lda = gensim.models.ldamodel.LdaModel
-ldamodel = Lda(doc_term_matrix, num_topics=3, id2word = dictionary, passes=50)
+	# Training LDA model
+	Lda = gensim.models.ldamodel.LdaModel
+	ldamodel = Lda(doc_term_matrix, num_topics=3, id2word = dictionary, passes=50)
 
-pairs = ldamodel.print_topics(num_topics=3, num_words=3)
-pairs_dictionary = {}
+	pairs = ldamodel.print_topics(num_topics=3, num_words=3)
+	pairs_dictionary = {}
 
-for i in range(0, len(pairs)):
-	for y in range(1, len(pairs[i])):
-		pairs_split = pairs[i][y].split(" + ")
-		for x in range(0, len(pairs_split)):
-			pairs_split2 = pairs_split[x].split("*")
-			pairs_split2[0] = float(pairs_split2[0])
-			pairs_split2[1] = pairs_split2[1][1:(len(pairs_split2[1])-1)]
-			pairs_dictionary[pairs_split2[1]] = pairs_split2[0]
+	for i in range(0, len(pairs)):
+		for y in range(1, len(pairs[i])):
+			pairs_split = pairs[i][y].split(" + ")
+			for x in range(0, len(pairs_split)):
+				pairs_split2 = pairs_split[x].split("*")
+				pairs_split2[0] = float(pairs_split2[0])
+				pairs_split2[1] = pairs_split2[1][1:(len(pairs_split2[1])-1)]
+				pairs_dictionary[pairs_split2[1]] = pairs_split2[0]
 
-# Decending
-most_popular_topics = sorted(pairs_dictionary, key=pairs_dictionary.get, reverse=True)
-print(most_popular_topics)
+	# Decending
+	most_popular_topics = sorted(pairs_dictionary, key=pairs_dictionary.get, reverse=True)
+	print(most_popular_topics)
+
+if __name__ == "__main__":
+	FindTopics()
