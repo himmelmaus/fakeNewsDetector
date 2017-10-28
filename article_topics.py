516c652b7cd9ae4546f6148fc262002e72241a41
Karolishp <31632511+KaroliShp@users.noreply.github.com>
2017-10-28 04:53:19 +0100

    Add files via upload

--- article_topics.py
+++ article_topics.py
@@ -7,18 +7,19 @@ import gensim # pip3 install Gensim
 from gensim import corpora
 
 # Base code from: https://www.analyticsvidhya.com/blog/2016/08/beginners-guide-to-topic-modeling-in-python/
-# Outputs around 6 words that describes the topic of the article
 
+# Outputs around 6 words that describes the topic of the article
 def FindTopics():
 	# Present the article in a sentence per new line
 	doc_complete = open("text.txt", "r").readlines()
 
+	# Lists of unnecessary words, used in def clean() to clean the text from these words
 	stop = set(stopwords.words('english'))
 	exclude = set(string.punctuation)
 	lemma = WordNetLemmatizer()
 	unnecessary = []
 
-	# Pos tag words and remove tags "IN", "MD", "CD"
+	# Pos tag words and remove tags "IN", "MD", "CD" (unnecessary words that often appear in the texts)
 	for i in range (0, len(doc_complete)):
 		doc_tokens = nltk.word_tokenize(doc_complete[i])
 		doc_pos_tags = nltk.pos_tag(doc_tokens)
@@ -45,6 +46,7 @@ def FindTopics():
 
 	# Training LDA model
 	Lda = gensim.models.ldamodel.LdaModel
+	# Number of topics - warries
 	ldamodel = Lda(doc_term_matrix, num_topics=3, id2word = dictionary, passes=50)
 
 	pairs = ldamodel.print_topics(num_topics=3, num_words=3)
@@ -61,7 +63,25 @@ def FindTopics():
 
 	# Decending
 	most_popular_topics = sorted(pairs_dictionary, key=pairs_dictionary.get, reverse=True)
-	print(most_popular_topics)
+	return most_popular_topics
+
+# Used to find names and any other "PERSON" tags in the text
+def FindNames():
+	doc_complete = open("text.txt", "r").readlines()
+	
+	names = []
+
+	for sentence in doc_complete:
+		doc_tokens = nltk.word_tokenize(sentence)
+		tagged_sentence = nltk.pos_tag(doc_tokens)
+		for chunk in nltk.ne_chunk(tagged_sentence):
+			if type(chunk) == nltk.tree.Tree:
+				if chunk.label() == 'PERSON':
+					names.append(' '.join([c[0] for c in chunk]))
+
+	names = list(set(names))
+	return names
 
 if __name__ == "__main__":
 	FindTopics()
+	FindNames()
