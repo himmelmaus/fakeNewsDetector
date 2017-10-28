ab909cf71344bd28db97a8aa39ebd9933e64d440
findlaysmith <ferelach45@gmail.com>
2017-10-28 04:25:32 +0100

    ly rokas

--- app/run.py
+++ app/run.py
@@ -4,14 +4,9 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route('/', methods = ['GET','POST'])
-def my_form():
-    return render_template("index.html")
-
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET','POST'])
 def print_form():
     if request.method == 'POST':
-        print (request.form(['input']))
         return render_template('index.html', result=request.form['input'])
     if request.method == 'GET':
         return render_template('index.html')
