cdd42204c0b544e50800d6ee227791b517f6c4ad
Karolishp <31632511+KaroliShp@users.noreply.github.com>
2017-10-28 07:20:44 +0100

    Add files via upload

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -2,7 +2,7 @@ import nltk
 from nltk.classify import NaiveBayesClassifier
 from nltk.classify.util import accuracy
 
-# Base code: https://www.twilio.com/blog/2017/09/sentiment-analysis-python-messy-data-nltk.html
+# Base code and data from: https://www.twilio.com/blog/2017/09/sentiment-analysis-python-messy-data-nltk.html
 
 def format_sentence(sent):
     return({word: True for word in nltk.word_tokenize(sent)})
@@ -21,9 +21,12 @@ with open("./TrainingData/neg_tweets.txt") as f:
 training = pos[:int((.8)*len(pos))] + neg[:int((.8)*len(neg))]
 test = pos[int((.8)*len(pos)):] + neg[int((.8)*len(neg)):]
 
+# Actual classifier
 classifier = NaiveBayesClassifier.train(training)
  
 doc_complete = open("text.txt", "r").readlines()
+
+#Calculating average of sentiments of the sentences
 avg = 0
 length = len(doc_complete)
 for items in doc_complete:
